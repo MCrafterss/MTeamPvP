574b6a187448a11020bd8ffd04fe019076f895eb
Taha_The_Hacker <tahasalah585@gmail.com>
2015-11-12 22:46:35 +0300

    Yea!

--- src/MCrafters/TeamPvP/TeamPvP.php
+++ src/MCrafters/TeamPvP/TeamPvP.php
@@ -5,138 +5,149 @@ namespace MCrafters\TeamPvP;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\utils\Config;
-use pocketmine\item\item;
-use pocketmine\event\Event;
-use pocketmine\level\Position;
 use pocketmine\event\Listener;
-use pocketmine\Server;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\level\Level;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
 use pocketmine\command\Command;
-use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
-use pocketmine\command\PluginCommand;
-use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\Player;
 
-class TeamPvP extends PluginBase implements Listener {
+class TeamPvP extends PluginBase implements Listener{
    
-   //Teams
+    // Teams
     public $red = [];
     public $blue = [];
     public $yml;
-    
-    
-  public function onEnable(){
-     //Initializing config files
-     
-      $this->saveResource("config.yml");
-      $yml = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-      $this->yml = $yml->getAll();
-      
-      $this->getLogger()->debug("Config files have been saved!");
-      
-      $this->getServer()->getPluginManager()->registerEvents($this, $this);
-      $this->getServer()->getLogger()->info(Color::BOLD . Color::GOLD . "M" . Color::AQUA . "TeamPvP " . Color::GREEN . "Enabled" . Color::RED . "!");
-    }
-    
-  public function isFriend($p1, $p2){
-    if(in_array($p1, $this->red, true) && in_array($p2, $this->red, true)){
-      return true;
-      
-    } else{
+
+    public function onEnable(){
+        // Initializing config files
+        $this->saveResource("config.yml");
+        $yml = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->yml = $yml->getAll();
        
-      return false;
-      
+        $this->getLogger()->debug("Config files have been saved!");
+       
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info(Color::BOLD . Color::GOLD . "M" . Color::AQUA . "TeamPvP " . Color::GREEN . "Enabled" . Color::RED . "!");
     }
-     if(in_array($p1, $this->blue, true) && in_array($p2, $this->blue, true)){
-       return true;
-     } else{
-       return false;
-     }
-   }//isFriend
 
-  public function getTeam($p){
-    if(in_array($p, $this->red, true)){
-      return "red";
-    } elseif(in_array($p, $this->blue, true)){
-      return "blue";
+    public function isFriend($p1, $p2){
+        if($this->getTeam($p1) === $this->getTeam($p2) && $this->getTeam($p1) !== false){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    // isFriend
+    public function getTeam($p){
+        if(in_array($p, $this->red)){
+            return "red";
+        }
+        elseif(in_array($p, $this->blue)){
+            return "blue";
+        }
+        else
+            return false;
+    }
+
+    public function setTeam($p, $team){
+        if(strtolower($team) === "red"){
+            if($this->getTeam($p) === "blue"){
+                unset($this->blue[$p]);
+            }
+            array_push($this->red, $p);
+        }
+        elseif(strtolower($team) === "blue"){
+            if($this->getTeam($p) === "red"){
+                unset($this->red[$p]);
+            }
+            array_push($this->blue, $p);
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event){
+        $teams = array("red","blue");
+        $b = $event->getBlock();
+        if($b->getX() === $this->yml["sign_join_x"] && $b->getY() === $this->yml["sign_join_y"] && $b->getZ() === $this->yml["sign_join_z"]){
+            if(count($this->red < 5) && count($this->blue < 5)){
+                $this->setTeam($event->getPlayer()->getName(), array_rand($teams, 1));
+                $event->getPlayer()->inGame = true;
+                $event->getPlayer()->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
+            }
+            elseif(count($this->red < 5)){
+                $this->setTeam($event->getPlayer()->getName(), "red");
+                $event->getPlayer()->inGame = true;
+                $event->getPlayer()->teleport(new Vector3($this->yml["red_enter_x"], $this->yml["red_enter_y"], $this->yml["red_enter_z"]));
+            }
+            elseif(count($this->blue) < 5){
+                $this->setTeam($event->getPlayer()->getName(), "blue");
+                $event->getPlayer()->inGame = true;
+                $event->getPlayer()->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
+            }
+            else{
+                $event->getPlayer()->sendMessage("Teams are full");
+            }
+        }
+    }
+
+    public function onEntityDamage(EntityDamageEvent $event){
+        if($event instanceof EntityDamageByEntityEvent){
+            if($event->getEntity() instanceof Player){
+                if($this->isFriend($event->getDamager()->getName(), $event->getEntity()->getName())){
+                    $event->setCancelled(true);
+                    $event->getDamager()->sendMessage($event->getEntity()->getName() . " is in your team!");
+                }
+                elseif(!$this->isFriend($event->getDamager()->getName(), $event->getEntity()->getName())){
+                    $event->getDamager()->sendMessage("You hit " . $event->getEntity()->getName());
+                }
+            }
+        }
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        $teams = array("red","blue");
+        switch($cmd->getName()){
+            case "team":
+                {
+                    switch(strtolower($args[0])){
+                        case "red":
+                            {
+                                if($sender instanceof Player){
+                                    $this->setTeam($sender->getName(), "red");
+                                    $sender->inGame = true;
+                                    $sender->teleport(new Vector3($this->yml["red_enter_x"], $this->yml["red_enter_y"], $this->yml["red_enter_z"]));
+                                    return true;
+                                }
+                                else
+                                    return false;
+                            }
+                        case "blue":
+                            {
+                                if($sender instanceof Player){
+                                    $this->setTeam($sender->getName(), "blue");
+                                    $sender->inGame = true;
+                                    $sender->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
+                                    return true;
+                                }
+                                else
+                                    return false;
+                            }
+                        case "var":
+                            {
+                                var_dump($this->red);
+                                var_dump($this->blue);
+                                return true;
+                            }
+                        default:
+                            {
+                    return false;
+                            }
+                }
     }
   }
-
-  public function setTeam($p, $team){
-    $red  = array_search($p, array_keys($this->red));
-    $blue = array_search($p, array_keys($this->blue));
-    
-    if(strtolower($team) === "red"){
-       if(isset($this->blue[$blue])){
-    unset($this->blue[$blue]);
-       }
-    array_push($this->red, $p);
-    }
-    
-    if(strtolower($team) === "blue"){
-       if(isset($this->red[$red])){
-      unset($this->red[$red]);
-       }
-    array_push($this->blue, $p);
-    }
-  }
-
-
-  public function Interact(PlayerInteractEvent $event){
-    $teams = array("red", "blue");
-    $b = $event->getBlock();
-    if($b->getX() === $this->yml["sign_join_x"] && $b->getY() === $this->yml["sign_join_y"] && $b->getZ() === $this->yml["sign_join_z"]){
-      if(count($this->red < 5) && count($this->blue < 5)){
-    $this->setTeam($event->getPlayer()->getName(), array_rand($teams, 1));
-
-      }elseif(count($this->red < 5)){
-      $this->setTeam($event->getPlayer()->getName(), "red");
-      $event->getPlayer()->inGame = true;
-      $event->getPlayer()->teleport(new Vector3($this->yml["red_enter_x"], $this->yml["red_enter_y"], $this->yml["red_enter_z"]));
-    } elseif(count($this->blue) < 5){
-      $this->setTeam($event->getPlayer()->getName(), "blue");
-      $event->getPlayer()->inGame = true;
-      $event->getPlayer()->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
-    } else {
-      $event->getPlayer()->sendMessage("Teams are full");
-    }
-    }
-  }
-
-public function EntityDamage(EntityDamageEvent $event){
-   if($event instanceof EntityDamageByEntityEvent){
-  if($this->isFriend($event->getDamager()->getName(), $event->getEntity()->getName()) === true){
-    $event->setCancelled(true);
-    $event->getDamager()->sendMessage($event->getEntity()->getName() . " is in your team!");
-  }
-}
-}
-
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    $teams = array("red", "blue");
-switch($cmd->getName()){
-      case "team":
-      if(count($this->red < 5) && count($this->blue < 5)){
-    $this->setTeam($sender->getName(), "red");
-      $sender->inGame = true;
-$sender->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
-      }elseif(count($this->red < 5)){
-      $this->setTeam($sender->getName(), "red");
-      $sender->inGame = true;
-      $sender->teleport(new Vector3($this->yml["red_enter_x"], $this->yml["red_enter_y"], $this->yml["red_enter_z"]));
-    } elseif(count($this->blue) < 5){
-      $this->setTeam($sender->getName(), "blue");
-      $sender->inGame = true;
-      $sender->teleport(new Vector3($this->yml["blue_enter_x"], $this->yml["blue_enter_y"], $this->yml["blue_enter_z"]));
-    } else {
-      $sender->sendMessage("Teams are full");
-    }
-}
 }
 }//Class
