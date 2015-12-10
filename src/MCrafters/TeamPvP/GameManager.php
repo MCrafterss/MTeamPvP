58542f60c574cf2a76b94eb96d5b3c1ca10b33fc
PocketKiller <tahasalah585@gmail.com>
2015-12-10 21:53:44 +0300

    Yay!

---
+++ src/MCrafters/TeamPvP/GameManager.php
@@ -0,0 +1,26 @@
+<?php
+namespace MCrafters\TeamPvP;
+
+use pocketmine\scheduler\ServerScheduler as Tasks;
+
+class GameManager
+{
+    public $reds;
+    public $blues;
+    public $gst;
+    public $gwt;
+
+    public function run()
+    {
+        $a = new TeamPvP();
+        $this->reds = $a->reds;
+        $this->blues = $a->blues;
+
+        if (count($this->reds) < 5 && count($this->blues) < 5) {
+            $this->gst = Tasks::scheduleRepeatingTask(new Tasks\GameStartTask($this), 20)->getTaskId();
+            Tasks::cancelTask($this->gwt);
+        } else {
+            $this->gwt = Tasks::scheduleRepeatingTask(new Tasks\GameWaitingTask($this), 15)->getTaskId();
+        }
+    }
+}
