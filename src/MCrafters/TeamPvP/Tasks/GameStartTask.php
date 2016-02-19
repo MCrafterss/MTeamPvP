89a44c867f5fce3454975e38300a226d5ad4fa9a
xBeastMode <xbeastmode00@gmail.com>
2016-02-19 09:15:00 -0800

    Optimize
    Small optimization. Item::fromString will item ids and names.

--- src/MCrafters/TeamPvP/Tasks/GameStartTask.php
+++ src/MCrafters/TeamPvP/Tasks/GameStartTask.php
@@ -6,7 +6,6 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\level\Level;
 use pocketmine\Player;
 use pocketmine\item\Item;
-use pocketmine\Server;
 use pocketmine\tile\Sign;
 use pocketmine\scheduler\Task;
 use pocketmine\scheduler\ServerScheduler as Tasks;
@@ -18,7 +17,7 @@ class GameStartTask extends PluginTask
 
     public $seconds = 15;
 
-    public function __construct(\MCrafters\TeamPvP\GameManager $plugin)
+    public function __construct(\MCrafters\TeamPvP\TeamPvP $plugin)
     {
         parent::__construct($plugin);
         $this->plugin = $plugin;
@@ -26,19 +25,19 @@ class GameStartTask extends PluginTask
 
     public function onRun($tick)
     {
-        $this->seconds = $this->seconds-1;
+        $this->seconds -= 1;
 
         foreach ($this->plugin->reds as $r) {
             foreach ($this->plugin->blues as $b) {
                 foreach ($this->plugin->yml["items"] as $i) {
-                    Server::getInstance()->getPlayer($r)->sendPopup("§eThe game will start in $this->seconds second(s)!");
-                    Server::getInstance()->getPlayer($b)->sendPopup("§eThe game will start in $this->seconds second(s)!");
+                    $this->plugin->getServer()->getPlayer($r)->sendPopup("§eThe game will start in {$this->seconds} ".($this->seconds<=1?"second":"seconds"));
+                    $this->plugin->getServer()->getPlayer($b)->sendPopup("§eThe game will start in {$this->seconds} ".($this->seconds<=1?"second":"seconds"));
 
                     if ($this->seconds == 1) {
-                        Server::getInstance()->getPlayer($r)->teleport(new Vector3($this->plugin->yml["red_enter_x"], $this->plugin->yml["red_enter_y"], $this->plugin->yml["red_enter_z"]));
-                        Server::getInstance()->getPlayer($b)->teleport(new Vector3($this->plugin->yml["blue_enter_x"], $this->plugin->yml["blue_enter_y"], $this->plugin->yml["blue_enter_z"]));
-                        Server::getInstance()->getPlayer($r)->getInventory()->addItem(Item::get($i));
-                        Server::getInstance()->getPlayer($b)->getInventory()->addItem(Item::get($i));
+                        getPlayer($r)->teleport(new Vector3($this->plugin->yml["red_enter_x"], $this->plugin->yml["red_enter_y"], $this->plugin->yml["red_enter_z"]));
+                        $this->plugin->getServer()->getPlayer($b)->teleport(new Vector3($this->plugin->yml["blue_enter_x"], $this->plugin->yml["blue_enter_y"], $this->plugin->yml["blue_enter_z"]));
+                        $this->plugin->getServer()->getPlayer($r)->getInventory()->addItem(Item::fromString($i));
+                        $this->plugin->getServer()->getPlayer($b)->getInventory()->addItem(Item::fromString($i));
                         $this->plugin->gameStarted = true;
                         $this->seconds = 15;
                         Tasks::cancelTask($this->getTaskId());
