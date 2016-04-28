11d210367437b94d8e9c2d19fa06d3b88f3a489b
Amir Emad <amir.emad9@gmail.com>
2016-04-28 02:38:51 +0200

    fixes #17

---
+++ src/MCrafters/TeamPvP/GameManager.php
@@ -0,0 +1,26 @@
+<?php
+namespace MCrafters\TeamPvP;
+use pocketmine\scheduler\ServerScheduler as Tasks;
+use pocketmine\Server;
+class GameManager
+{
+    public $reds;
+    public $blues;
+    public $gst;
+    public $gwt;
+     
+    public function run()
+    {
+        $team = new \MCrafters\TeamPvP\TeamPvP();
+        $manager = new \MCrafters\TeamPvP\GameManager($team);
+        $this->plugin = $team;
+        $this->reds = $this->plugin->reds;
+        $this->blues = $this->plugin->blues;
+        if (count($this->reds) == 5 && count($this->blues) == 5) {
+            $this->gst = Server::getInstance()->getScheduler()->scheduleRepeatingTask(new \MCrafters\TeamPvP\Tasks\GameStartTask($this), 20)->getTaskId();
+            Server::getInstance()->getScheduler()->cancelTask($this->gwt);
+        } else {
+            $this->gwt = Server::getInstance()->getScheduler()->scheduleRepeatingTask(new \MCrafters\TeamPvP\Tasks\GameWaitingTask($this), 15)->getTaskId();
+        }
+    }
+}
